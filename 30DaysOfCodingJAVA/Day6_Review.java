21a3ab08b46f6cea7c3afdf9f7b3a563fc1fc6ed
aalexandrov11 <78275303+aalexandrov11@users.noreply.github.com>
2021-07-09 00:52:47 +0300

    Update Day6_Review.java

--- 30DaysOfCodingJAVA/Day6_Review.java
+++ 30DaysOfCodingJAVA/Day6_Review.java
@@ -1,1 +1,23 @@
+public class Solution {
 
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+        for(int i = 0; i < n; i++){
+            String even = "";
+            String odd = "";
+            String temp = scan.nextLine();
+            for(int j = 0; j < temp.length(); j++){
+                if(j % 2 == 0){
+                    even += temp.substring(j, j + 1);
+                } else{
+                    odd += temp.substring(j, j + 1);
+                }
+            }
+            System.out.println(even + " " + odd);
+        }
+        scan.close();
+    }
+}
