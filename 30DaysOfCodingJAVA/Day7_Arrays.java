9548b2ca7f413491ea582272998e87b0ff5b2d0e
aalexandrov11 <78275303+aalexandrov11@users.noreply.github.com>
2021-07-09 01:48:23 +0300

    Update Day7_Arrays.java

--- 30DaysOfCodingJAVA/Day7_Arrays.java
+++ 30DaysOfCodingJAVA/Day7_Arrays.java
@@ -1,1 +1,21 @@
+public class Solution {
+    public static void reverse(int n, List<Integer> arr){
+        for(int i = n - 1; i >= 0; i--){
+            System.out.print(arr.get(i) + " ");
+        }
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+        
+        reverse(n, arr);
+        
+        bufferedReader.close();
+    }
+}
