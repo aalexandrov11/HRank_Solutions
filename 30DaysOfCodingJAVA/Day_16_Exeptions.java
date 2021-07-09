3cd6cdd418942788750023bb56f757331854505b
aalexandrov11 <78275303+aalexandrov11@users.noreply.github.com>
2021-07-09 21:20:17 +0300

    Update Day_16_Exeptions.java

--- 30DaysOfCodingJAVA/Day_16_Exeptions.java
+++ 30DaysOfCodingJAVA/Day_16_Exeptions.java
@@ -1,1 +1,16 @@
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
+        String S = bufferedReader.readLine();
+        
+        try{
+            System.out.println(Integer.parseInt(S));
+        }
+        catch(Exception e){
+            System.out.println("Bad String");
+        }
+        //finally{}
+        bufferedReader.close();
+    }
+}
