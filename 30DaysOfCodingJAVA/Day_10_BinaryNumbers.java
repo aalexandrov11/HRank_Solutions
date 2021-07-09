f85a01779837aa64f6b48c8a0d120568c2125b9e
aalexandrov11 <78275303+aalexandrov11@users.noreply.github.com>
2021-07-09 16:37:08 +0300

    Update Day_10_BinaryNumbers.java

--- 30DaysOfCodingJAVA/Day_10_BinaryNumbers.java
+++ 30DaysOfCodingJAVA/Day_10_BinaryNumbers.java
@@ -1,1 +1,46 @@
+public class Solution {
+    public static String convertToBinary(int n){
+        int remain = 0;
+        StringBuilder temp = new StringBuilder();
+        while(n > 0){
+            remain = n % 2;
+            n /= 2;
+            temp.append(remain);
+        }
+        
+        char[] result = new char[temp.length()];
+        //System.out.println(result.toString);
+        //System.out.println(temp.length());
+        for(int i = 0, j = temp.length() - 1; i < temp.length(); i++, j--){
+            result[i] = temp.charAt(j);
+            
+        }
+        
+        return new String(result);//String.valueOf(result); - sushto stava // ako se izpolzva result.toString() shte predade adresa, a ne stoinostta
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+        String binaryNumber = convertToBinary(n);
+
+        int result = 0;
+        int temp = 0;
+        for(int i = 0; i < binaryNumber.length(); i++){
+            
+            if(binaryNumber.charAt(i) == '1'){
+                temp++;
+                if(temp > result){
+                    result = temp;
+                }
+            }else{
+                temp = 0;
+            }
+        }
+        
+        System.out.println(result);
+        
+        bufferedReader.close();
+    }
+}
