1c0a5928b0e7becd4bad365ac018f92dd1c536aa
aalexandrov11 <78275303+aalexandrov11@users.noreply.github.com>
2021-07-10 17:54:41 +0300

    Update Day_25_RunningTimeAndComplexity.java

--- 30DaysOfCodingJAVA/Day_25_RunningTimeAndComplexity.java
+++ 30DaysOfCodingJAVA/Day_25_RunningTimeAndComplexity.java
@@ -1,1 +1,31 @@
-
+public class Solution {
+    
+    static void isPrime(int n){
+        boolean flag = true;
+        if(n == 1){
+            flag = false;
+        }else{
+            for(int i = 2; i <= Math.sqrt(n); i++){
+                if(n % i == 0){
+                    flag = false;
+                }
+            }
+        }
+        
+        if(flag == true){
+            System.out.println("Prime");
+        }else{
+            System.out.println("Not prime");
+        }
+    }
+    
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        int t = Integer.parseInt(scan.nextLine());
+        for(int i = 0; i < t; i++){
+            int n = Integer.parseInt(scan.nextLine());
+            isPrime(n);
+        }
+    }
+}
