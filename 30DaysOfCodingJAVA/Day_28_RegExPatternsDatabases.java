814ca6d005ac6e42c80fd06b1890b6ee7bce0641
aalexandrov11 <78275303+aalexandrov11@users.noreply.github.com>
2021-07-11 00:35:29 +0300

    Update Day_28_RegExPatternsDatabases.java

--- 30DaysOfCodingJAVA/Day_28_RegExPatternsDatabases.java
+++ 30DaysOfCodingJAVA/Day_28_RegExPatternsDatabases.java
@@ -1,1 +1,35 @@
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        List<String> names = new LinkedList<String>();
+        String mail = ".+@gmail\\.com$";
+        IntStream.range(0, N).forEach(NItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                String firstName = firstMultipleInput[0];
+
+                String emailID = firstMultipleInput[1];
+                
+                if(Pattern.matches(mail, emailID)){
+                names.add(firstName);
+                }
+                
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+        
+        
+        
+        Collections.sort(names);
+        
+        for(String name : names){
+            System.out.println(name);
+        }
+
+        bufferedReader.close();
+    }
+}
