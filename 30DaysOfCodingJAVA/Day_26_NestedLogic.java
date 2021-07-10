01bbb15409cd2e7999cdc18feafe3c6d56daefa6
aalexandrov11 <78275303+aalexandrov11@users.noreply.github.com>
2021-07-10 20:40:01 +0300

    Update Day_26_NestedLogic.java

--- 30DaysOfCodingJAVA/Day_26_NestedLogic.java
+++ 30DaysOfCodingJAVA/Day_26_NestedLogic.java
@@ -1,1 +1,32 @@
+public class Solution {
 
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        int[] returned = new int[3];
+        int[] due = new int[3];
+        returned = Arrays.stream(scan.nextLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+                    
+        due = Arrays.stream(scan.nextLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+                    
+        if(returned[2] < due[2]){
+            System.out.println("0");
+        }else if(returned[2] == due[2]){
+            if(returned[0] <= due[0] && returned[1] <= due[1]){
+                System.out.println("0");
+            }else if(returned[0] > due[0] && returned[1] == due[1]){
+                System.out.println(15 * (returned[0] - due[0]));
+            }else{
+                System.out.println(500 * (returned[1] - due[1]));
+            }
+        }else{
+            System.out.println("10000");
+        }
+        
+        scan.close();
+    }
+}
